73289cb5ebc6950ca0acf5e3f5dbbc009109c7c3
Miguel Palau <mpalauzarza@gmail.com>
2018-03-19 10:32:39 -0600

    quick linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,61 @@
-var pkg      = require('./package.json'),
-	pug      = require('gulp-pug'),
-	gulp     = require('gulp'),
-	less     = require('gulp-less'),
-	clean    = require('gulp-clean-css'),
-	uglify   = require('gulp-uglify'),
-	rename   = require('gulp-rename'),
-	header   = require('gulp-header'),
-	jshint   = require('gulp-jshint'),
-	stylish  = require('jshint-stylish');
+var pkg = require('./package.json')
+var pug = require('gulp-pug')
+var gulp = require('gulp')
+var less = require('gulp-less')
+var clean = require('gulp-clean-css')
+var uglify = require('gulp-uglify')
+var rename = require('gulp-rename')
+var header = require('gulp-header')
+var jshint = require('gulp-jshint')
+var stylish = require('jshint-stylish');
 
 var banner = ['/**',
-	' * Vanilla JavaScript Accordion v<%= pkg.version %>',
-	' * <%= pkg.homepage %>',
-	' */',
-	''].join('\n');
+  ' * Vanilla JavaScript Accordion v<%= pkg.version %>',
+  ' * <%= pkg.homepage %>',
+  ' */',
+  ''].join('\n');
 
-gulp.task('script', function() {
-	gulp.src(['./src/javascript/vanilla-js-accordion.js'])
-		.pipe(uglify())
-		.pipe(header(banner, { 
-			pkg: pkg 
-		}))
-		.pipe(rename({ 
-			suffix: '.min' 
-		}))
-		.pipe(gulp.dest('./dist'))
-		.pipe(gulp.dest('./docs/javascript'));
+gulp.task('script', function () {
+  gulp.src(['./src/javascript/vanilla-js-accordion.js'])
+    .pipe(uglify())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./docs/javascript'));
 });
 
-gulp.task('markup', function() {
-	gulp.src('./src/index.pug')
-		.pipe(pug({
-			pretty: true
-		}))
-		.pipe(gulp.dest('./dist'));
+gulp.task('markup', function () {
+  gulp.src('./src/index.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('styles', function() {
-	gulp.src('./src/styles/*.less')
-		.pipe(less())
-		.pipe(gulp.dest('./dist'))
-		.pipe(gulp.dest('./docs/styles'));
+gulp.task('styles', function () {
+  gulp.src('./src/styles/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./docs/styles'));
 });
 
-gulp.task('docs-styles', function() {
-	gulp.src('./docs/styles/*.less')
-		.pipe(less())
-		.pipe(clean({ 
-			compatibility: 'ie9' 
-		}))
-		.pipe(gulp.dest('./docs/styles'));
+gulp.task('docs-styles', function () {
+  gulp.src('./docs/styles/*.less')
+    .pipe(less())
+    .pipe(clean({
+      compatibility: 'ie9'
+    }))
+    .pipe(gulp.dest('./docs/styles'));
 });
 
-gulp.task('lint', function() {
-	return gulp.src('./src/javascript/*.js')
-		.pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter(stylish));
+gulp.task('lint', function () {
+  return gulp.src('./src/javascript/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter(stylish));
 });
 
 gulp.task('default', [ 'script', 'markup', 'styles', 'docs-styles', 'lint' ]);
--- src/javascript/vanilla-js-accordion.js
+++ src/javascript/vanilla-js-accordion.js
@@ -13,125 +13,124 @@
 * @param {number} [options.openTab=1] - Start the accordion with this item opened.
 * @param {boolean} [options.oneOpen=false] - Only one tab can be opened at a time.
 */
-var Accordion = function(options) {
-    var element = document.getElementById(options.element),
-        openTab = options.openTab,
-        oneOpen = options.oneOpen || false,
+var Accordion = function (options) {
+  var element = document.getElementById(options.element)
+  var openTab = options.openTab
+  var oneOpen = options.oneOpen || false
 
-        titleClass   = 'js-Accordion-title',
-        contentClass = 'js-Accordion-content';
-        
-    render();
-    
-    /**
+  var titleClass = 'js-Accordion-title'
+  var contentClass = 'js-Accordion-content'
+
+  render();
+
+  /**
      * Initial rendering of the accordion.
      */
-    function render() {
-        // attach classes to buttons and containers
-        [].forEach.call(element.parentElement.querySelectorAll('#' + options.element + '> button'), 
-            function(item) {
-                item.classList.add(titleClass);
-                item.nextElementSibling.classList.add(contentClass);
-            });
+  function render () {
+    // attach classes to buttons and containers
+    [].forEach.call(element.parentElement.querySelectorAll('#' + options.element + '> button'),
+      function (item) {
+        item.classList.add(titleClass);
+        item.nextElementSibling.classList.add(contentClass);
+      });
 
-        // attach only one click listener
-        element.addEventListener('click', onClick);
+    // attach only one click listener
+    element.addEventListener('click', onClick);
 
-        // accordion starts with all tabs closed
-        closeAll();
+    // accordion starts with all tabs closed
+    closeAll();
 
-        // sets the open tab - if defined
-        if (openTab) { 
-            open(openTab);
-        }
+    // sets the open tab - if defined
+    if (openTab) {
+      open(openTab);
     }
+  }
 
-    /**
+  /**
      * Handles clicks on the accordion.
-     * 
+     *
      * @param {object} e - Element the click occured on.
      */
-    function onClick(e) {
-        if (e.target.className.indexOf(titleClass) === -1) {
-            return;
-        }
-        
-        if (oneOpen) {
-            closeAll();
-        }
-
-        toggle(e.target.nextElementSibling);
+  function onClick (e) {
+    if (e.target.className.indexOf(titleClass) === -1) {
+      return;
     }
-    
-    /**
+
+    if (oneOpen) {
+      closeAll();
+    }
+
+    toggle(e.target.nextElementSibling);
+  }
+
+  /**
      * Closes all accordion tabs.
      */
-    function closeAll() {
-        [].forEach.call(element.querySelectorAll('.' + contentClass), function(item) {
-            item.style.height = 0;
-        });
-    }
-    
-    /**
+  function closeAll () {
+    [].forEach.call(element.querySelectorAll('.' + contentClass), function (item) {
+      item.style.height = 0;
+    });
+  }
+
+  /**
      * Toggles corresponding tab for each title clicked.
-     * 
+     *
      * @param {object} el - The content tab to show or hide.
      */
-    function toggle(el) {
-        // getting the height every time in case
-        // the content was updated dynamically
-        var height = el.scrollHeight;
+  function toggle (el) {
+    // getting the height every time in case
+    // the content was updated dynamically
+    var height = el.scrollHeight;
 
-        if (el.style.height === '0px' || el.style.height === '') {
-            el.style.height = height + 'px';
-        } else {
-            el.style.height = 0;
-        }
+    if (el.style.height === '0px' || el.style.height === '') {
+      el.style.height = height + 'px';
+    } else {
+      el.style.height = 0;
     }
+  }
 
-
-    /**
+  /**
      * Returns the corresponding accordion content element by index.
      *
      * @param {number} n - Index of tab to return
      */
-    function getTarget(n) {
-        return element.querySelectorAll('.' + contentClass)[n - 1];
-    }
-    
-    /**
+  function getTarget (n) {
+    return element.querySelectorAll('.' + contentClass)[n - 1];
+  }
+
+  /**
      * Opens a tab by index.
-     * 
+     *
      * @param {number} n - Index of tab to open.
-     * 
+     *
      * @public
      */
-    function open(n) {
-        var target = getTarget(n);
+  function open (n) {
+    var target = getTarget(n);
 
-        if (target) {
-            if (oneOpen) closeAll();
-            target.style.height = target.scrollHeight + 'px';
-        }
+    if (target) {
+      if (oneOpen) closeAll();
+      target.style.height = target.scrollHeight + 'px';
     }
+  }
 
-    /**
+  /**
      * Closes a tab by index.
-     * 
+     *
      * @param {number} n - Index of tab to close.
-     * 
+     *
      * @public
      */
-    function close(n) {
-        var target = getTarget(n);
+  function close (n) {
+    var target = getTarget(n);
 
-        if (target) {
-            target.style.height = 0;
-        }
+    if (target) {
+      target.style.height = 0;
     }
-    
-    return {
-        open: open,
-        close: close
-    };
+  }
+
+  return {
+    open: open,
+    close: close
+  };
 };
